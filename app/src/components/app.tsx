94cfe229f4587386ea034f58a0ce7b47d56a883e
joneverard <joneverard3@gmail.com>
2020-03-29 17:06:53 +0100

    initial commit of client

---
+++ app/src/components/app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./app.module.scss";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
+
+// things we need
+// header component - where are we and navigation
+// main container
+// three columns
+// three list components
+// ... with very different requirements (although there is probably a lot we can do here to reuse code)
