9a4c67839ee2d51ab3e2b3c8445678de66468681
joneverard <joneverard3@gmail.com>
2020-05-08 14:41:13 +0100

    add checkboxes

--- client/src/components/playlist-header/playlist-header.tsx
+++ client/src/components/playlist-header/playlist-header.tsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Checkbox, Button, Select, Input } from "antd";
 
 import styles from "./playlist-header.module.scss";
 
+const { Option } = Select;
+// what does this component need.
+// set of "actions" to place at the top.
+
 const PlaylistHeader = () => {
-  //
-  return <div className={styles.container}>{"hello"}</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <Checkbox />
+        {"Select all"}
+      </div>
+      <div>
+        <SelectItems />
+      </div>
+      <div>
+        <Button type="primary">Save</Button>
+      </div>
+    </div>
+  );
 };
 
 export default PlaylistHeader;
+
+const SelectItems = () => {
+  return (
+    <Input.Group compact>
+      <Select defaultValue="Option1-1">
+        <Option value="Option1-1">Option1-1</Option>
+        <Option value="Option1-2">Option1-2</Option>
+      </Select>
+      <Select defaultValue="Option2-2">
+        <Option value="Option2-1">Option2-1</Option>
+        <Option value="Option2-2">Option2-2</Option>
+      </Select>
+    </Input.Group>
+  );
+};
